7c74956a01e6d86452a7144b3b9f3aa0dd678729
leroy <dev@leroytop.com>
2026-04-13 10:52:00 +0800

    chore: update version to 1.0.2

--- src/version.h
+++ src/version.h
@@ -1,12 +1,12 @@
 /*
  * @Author: leroy
  * @Date: 2026-03-18 16:16:13
- * @LastEditTime: 2026-04-13 10:49:35
+ * @LastEditTime: 2026-04-13 10:51:56
  * @Description: Version header file
  */
 #ifndef VERSION_H_INCLUDED
 #define VERSION_H_INCLUDED
 
-#define VERSION "v1.0.1"
+#define VERSION "v1.0.2"
 
 #endif // VERSION_H_INCLUDED
